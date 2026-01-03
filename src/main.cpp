297512d79fa96bc4fb0986aff10f02d22a65258d
kapt0xa <kuzmich.ml@gmail.com>
2026-01-03 15:06:02 +0300

    img and font works for ubuntu

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,3 @@
-#include <SFML/Audio.hpp>
 #include <SFML/Graphics.hpp>
  
 int main()
@@ -7,18 +6,12 @@ int main()
     sf::RenderWindow window(sf::VideoMode({800, 600}), "SFML window");
  
     // Load a sprite to display
-    const sf::Texture texture("cute_image.jpg");
+    const sf::Texture texture("../resrc/cute_image.jpg");
     sf::Sprite sprite(texture);
  
     // Create a graphical text to display
-    const sf::Font font("arial.ttf");
+    const sf::Font font("../resrc/UbuntuSansMono[wght].ttf");
     sf::Text text(font, "Hello SFML", 50);
- 
-    // Load a music to play
-    sf::Music music("nice_music.ogg");
- 
-    // Play the music
-    music.play();
  
     // Start the game loop
     while (window.isOpen())
