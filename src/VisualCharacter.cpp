321e2e062aab924f4c68ae5b70e563f6c321f2ff
kapt0xa <kuzmich.ml@gmail.com>
2026-01-26 23:42:44 +0300

    ...

--- src/VisualCharacter.cpp
+++ src/VisualCharacter.cpp
@@ -1,19 +1,23 @@
 #include "VisualCharacter.h"
 #include <sstream>
+#include <climits>
+#include <cassert>
 
 namespace spiel
 {
     namespace binarySaveLoad
     {
-        struct FloatWrapper
+        template <typename T>
+        struct TWrapper
         {
-            float value;
-            FloatWrapper(float&& v) : value(v) {}
+            T value;
+            TWrapper(T&& v) : value(v) {}
         };
-        struct FloatRefWrapper
+        template <typename T>
+        struct TRefWrapper
         {
-            float& value;
-            FloatRefWrapper(float& v) : value(v) {}
+            T& value;
+            TRefWrapper(T& v) : value(v) {}
         };
         std::ostream& operator<<(std::ostream& out, const sf::Vector2f& vec)
         {
@@ -27,12 +31,14 @@ namespace spiel
             out.write(reinterpret_cast<const char*>(&colorInt), sizeof(colorInt));
             return out;
         }
-        std::ostream& operator<<(std::ostream& out, FloatWrapper value)
+        template <typename T>
+        std::ostream& operator<<(std::ostream& out, const TWrapper<T>& value)
         {
             out.write(reinterpret_cast<const char*>(&value.value), sizeof(value.value));
             return out;
         }
-        std::ostream& operator<<(std::ostream& out, FloatRefWrapper value)
+        template <typename T>
+        std::ostream& operator<<(std::ostream& out, TRefWrapper<T>&& value)
         {
             out.write(reinterpret_cast<const char*>(&value.value), sizeof(value.value));
             return out;
@@ -50,64 +56,173 @@ namespace spiel
             color = sf::Color(colorInt);
             return in;
         }
-        std::istream& operator>>(std::istream& in, FloatWrapper& value)
+        template <typename T>
+        std::istream& operator>>(std::istream& in, TWrapper<T>& value)
         {
             in.read(reinterpret_cast<char*>(&value.value), sizeof(value.value));
             return in;
         }
-        std::istream& operator>>(std::istream& in, FloatRefWrapper& value)
+        template <typename T>
+        std::istream& operator>>(std::istream& in, TRefWrapper<T>&& value)
         {
             in.read(reinterpret_cast<char*>(&value.value), sizeof(value.value));
             return in;
         }
+    }
+    void saveByteShape(std::ostream& out, sf::Shape& shape)
+    {
+        using namespace binarySaveLoad;
+        // Color          m_fillColor{Color::White};    //!< Fill color
+        // Color          m_outlineColor{Color::White}; //!< Outline color
+        // float          m_outlineThickness{};         //!< Thickness of the shape's outline
+        // Vector2f          m_origin;                           //!< Origin of translation/rotation/scaling of the object
+        // Vector2f          m_position;                         //!< Position of the object in the 2D world
+        // Angle             m_rotation;                         //!< Orientation of the object
+        // Vector2f          m_scale{1, 1}; 
+
+        out << shape.getFillColor()
+            << shape.getOutlineColor()
+            << TWrapper(shape.getOutlineThickness())
+            << shape.getOrigin()
+            << shape.getPosition()
+            << TWrapper(shape.getRotation().asRadians())
+            << shape.getScale();
+    }
+    void loadByteShape(std::istream& in, sf::Shape& shape)
+    {
+        using namespace binarySaveLoad;
+
+        sf::Color fillColor;
+        sf::Color outlineColor;
+        float outlineThickness;
+        sf::Vector2f origin;
+        sf::Vector2f position;
+        float rotationRadians;
+        sf::Vector2f scale;
+
+        in >> fillColor
+           >> outlineColor
+           >> TRefWrapper(outlineThickness)
+           >> origin
+           >> position
+           >> TRefWrapper(rotationRadians)
+           >> scale;
+
+        shape.setFillColor(fillColor);
+        shape.setOutlineColor(outlineColor);
+        shape.setOutlineThickness(outlineThickness);
+        shape.setOrigin(origin);
+        shape.setPosition(position);
+        shape.setRotation(sf::radians(rotationRadians));
+        shape.setScale(scale);
     }
     void VisualDetail::saveByte(std::ostream& out)
     {
+        using namespace binarySaveLoad;
+
         uint16_t dataSize;
+        uint16_t shapeType;
+        std::stringstream buf;
+
         if(!shape)
         {
-            dataSize = 4;
-            uint16_t shapeType = shapeTypeNone;
-            out.write(reinterpret_cast<const char*>(&dataSize), sizeof(dataSize));
-            out.write(reinterpret_cast<const char*>(&shapeType), sizeof(shapeType));
-            return;
+            dataSize = static_cast<uint16_t>(sizeof(shapeType) + sizeof(dataSize));
+            shapeType = shapeTypeNone;
         }
-
-        if(auto circle = dynamic_cast<sf::CircleShape*>(shape.get()))
+        else if(auto circle = dynamic_cast<sf::CircleShape*>(shape.get()))
         {
-            uint16_t shapeType = shapeTypeCircle;
-            out.write(reinterpret_cast<const char*>(&shapeType), sizeof(shapeType));
-            float radius = circle->getRadius();
-            out.write(reinterpret_cast<const char*>(&radius), sizeof(radius));
-            uint32_t pointCount = static_cast<uint32_t>(circle->getPointCount());
-            out.write(reinterpret_cast<const char*>(&pointCount), sizeof(pointCount));
-            return;
+            shapeType = shapeTypeCircle;
+            buf << TWrapper(circle->getRadius())
+                << TWrapper(static_cast<uint32_t>(circle->getPointCount()));
+            saveByteShape(buf, *circle);
         }
         else if(auto rectangle = dynamic_cast<sf::RectangleShape*>(shape.get()))
         {
-            uint16_t shapeType = shapeTypeRectangle;
-            out.write(reinterpret_cast<const char*>(&shapeType), sizeof(shapeType));
-            sf::Vector2f size = rectangle->getSize();
-            out.write(reinterpret_cast<const char*>(&size), sizeof(size));
-            return;
+            shapeType = shapeTypeRectangle;
+            buf << TWrapper(rectangle->getSize());
+            saveByteShape(buf, *rectangle);
         }
         else if(auto polygon = dynamic_cast<sf::ConvexShape*>(shape.get()))
         {
-            uint16_t shapeType = shapeTypePolygon;
-            out.write(reinterpret_cast<const char*>(&shapeType), sizeof(shapeType));
+            shapeType = shapeTypePolygon;
             uint32_t pointCount = static_cast<uint32_t>(polygon->getPointCount());
-            out.write(reinterpret_cast<const char*>(&pointCount), sizeof(pointCount));
+            buf << TRefWrapper(pointCount);
+            assert(pointCount <= UINT32_MAX);
             for(uint32_t i = 0; i < pointCount; ++i)
             {
-                sf::Vector2f point = polygon->getPoint(i);
-                out.write(reinterpret_cast<const char*>(&point), sizeof(point));
+                buf << polygon->getPoint(i);
             }
-            return;
+            saveByteShape(buf, *polygon);
         }
-        // Unknown shape type
-        throw std::runtime_error("VisualDetail::saveByte: Unknown shape type");
+        else
+        {
+            // Unknown shape type
+            throw std::runtime_error("VisualDetail::saveByte: Unknown shape type");
+        }
+        std::string bufStr = buf.str();
+        dataSize = static_cast<uint16_t>(bufStr.size() + sizeof(shapeType) + sizeof(dataSize));
+        assert(bufStr.size() < static_cast<size_t>(dataSize));
+        out << TRefWrapper(dataSize)
+            << TRefWrapper(shapeType)
+            << bufStr;
     }
-    bool VisualDetail::loadByte(std::istream& in);
+    bool VisualDetail::loadByte(std::istream& in)
+    {
+        check this copilot written
+        // using namespace binarySaveLoad;
+
+        // uint16_t dataSize;
+        // uint16_t shapeType;
+
+        // in >> TRefWrapper(dataSize)
+        //    >> TRefWrapper(shapeType);
+
+        // switch(shapeType)
+        // {
+        //     case shapeTypeNone:
+        //     {
+        //         setNone();
+        //         break;
+        //     }
+        //     case shapeTypeCircle:
+        //     {
+        //         float radius;
+        //         uint32_t pointCount;
+        //         in >> TRefWrapper(radius)
+        //            >> TRefWrapper(pointCount);
+        //         auto& circle = setCircle(radius, static_cast<int>(pointCount));
+        //         loadByteShape(in, circle);
+        //         break;
+        //     }
+        //     case shapeTypeRectangle:
+        //     {
+        //         sf::Vector2f size;
+        //         in >> TRefWrapper(size);
+        //         auto& rectangle = setRectangle(sf::Rect<float>({0, 0}, size));
+        //         loadByteShape(in, rectangle);
+        //         break;
+        //     }
+        //     case shapeTypePolygon:
+        //     {
+        //         uint32_t pointCount;
+        //         in >> TRefWrapper(pointCount);
+        //         std::vector<Vec2f> points(pointCount);
+        //         for(uint32_t i = 0; i < pointCount; ++i)
+        //         {
+        //             in >> points[i];
+        //         }
+        //         auto& polygon = setPolygon(points);
+        //         loadByteShape(in, polygon);
+        //         break;
+        //     }
+        //     default:
+        //     {
+        //         // Unknown shape type
+        //         throw std::runtime_error("VisualDetail::loadByte: Unknown shape type");
+        //     }
+        // }
+        // return true;
+    }
 
     void VisualDetail::setNone()
     {
