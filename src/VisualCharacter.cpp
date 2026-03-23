8735d7b725559258277f8fb9b80337a7f89ad1b8
kapt0xa (Igor Kuzmich) <kuzmich.ml@gmail.com>
2026-03-23 08:09:41 +0300

    ...

--- src/VisualCharacter.cpp
+++ src/VisualCharacter.cpp
@@ -2,4 +2,61 @@
 
 namespace spiel
 {
+        sf::ConvexShape Shape::getDrawable() const
+        {
+            sf::ConvexShape result;
+            result.setFillColor(color);
+            result.setPointCount(vertices.size());
+            for (size_t i = 0; i < vertices.size(); ++i)
+                result.setPoint(i, vectorCast::toSFML(vertices[i]));
+            return result;
+        }
+
+        Shape& Shape::setColor(const sf::Color& color)
+        {
+            this->color = color;
+            return *this;
+        }
+        Shape& Shape::reserve(size_t size)
+        {
+            vertices.reserve(size);
+            return *this;
+        }
+        Shape& Shape::setPointCount(size_t size)
+        {
+            vertices.resize(size);
+            return *this;
+        }
+        Shape& Shape::pushBack(const Vec2f& point)
+        {
+            vertices.push_back(point);
+            return *this;
+        }
+
+        Shape& Shape::scale(const Vec2f& scale)
+        {
+            for (auto& vertex : vertices)
+            {
+                X(vertex) *= X(scale);
+                Y(vertex) *= Y(scale);
+            }
+            return *this;
+        }
+        Shape& Shape::scale(float scale)
+        {
+            return this->scale(Vec2f(scale, scale));
+        }
+        Shape& Shape::rotate(float radians)
+        {
+            Vec2f complex(std::cos(radians), std::sin(radians));
+            return this->rotate(complex);
+        }
+        Shape& Shape::rotate(const Vec2f& complex)
+        {
+            for (auto& vertex : vertices)
+            {
+                vertex *= complex;
+            }
+            return *this;
+        }
 }
--- src/VisualCharacter.h
+++ src/VisualCharacter.h
@@ -12,4 +12,31 @@
 
 namespace spiel
 {
+    class Shape
+    {
+    public:
+        Shape() = default;
+        ~Shape() = default;
+        Shape(const Shape& other) = default;
+        Shape(Shape&& other) noexcept = default;
+        Shape& operator=(const Shape& other) = default;
+        Shape& operator=(Shape&& other) noexcept = default;
+
+        sf::ConvexShape getDrawable() const;
+        json::Node toJson() const;
+
+        Shape& setColor(const sf::Color& color);
+        Shape& reserve(size_t size);
+        Shape& setPointCount(size_t size);
+        Shape& pushBack(const Vec2f& point);
+
+        Shape& scale(const Vec2f& scale);
+        Shape& scale(float scale);
+        Shape& rotate(float radians);
+        Shape& rotate(const Vec2f& complex);
+
+    private:
+        sf::Color color;
+        std::vector<Vec2f> vertices;
+    };
 }
