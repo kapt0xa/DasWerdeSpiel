12e488396fbb5029da5c9f879ead38ce0f1026e1
kapt0xa <kuzmich.ml@gmail.com>
2026-03-29 11:46:42 +0300

    ...

--- src/CharacterEditor.cpp
+++ src/CharacterEditor.cpp
@@ -2,4 +2,38 @@
 
 namespace spiel
 {
+    void CharacterEditor::main(sf::RenderWindow& window, tgui::Gui& gui)
+    {
+        auto button = tgui::Button::create("Click to Exit");
+        button->setPosition("50%", "50%");
+        button->setOrigin(0.5f, 0.5f);
+        button->setSize("200", "50");
+
+        button->onPress([&](){ window.close(); });
+
+        button->getRenderer()->setBackgroundColor(sf::Color::Black);
+        button->getRenderer()->setTextColor(sf::Color::Green);
+        button->getRenderer()->setTextColorHover(sf::Color::Green);
+        button->getRenderer()->setBackgroundColorHover(sf::Color(7, 0, 15));
+        button->getRenderer()->setTextColorDown(sf::Color::Green);
+        button->getRenderer()->setBackgroundColorDown(sf::Color(31, 0, 63));
+
+        gui.add(button);
+
+        while (window.isOpen())
+        {
+            while (auto event = window.pollEvent())
+            {
+                if(gui.handleEvent(event.value()))
+                    continue;
+
+                if (event->getIf<sf::Event::Closed>())
+                    window.close();
+            }
+
+            window.clear();
+            gui.draw();
+            window.display();
+        }
+    }
 }
--- src/CharacterEditor.h
+++ src/CharacterEditor.h
@@ -1,10 +1,17 @@
 #pragma once
 #include "VisualCharacter.h"
 
+#include <SFML/Graphics.hpp>
+#include <TGUI/TGUI.hpp>
+#include <TGUI/Backend/SFML-Graphics.hpp>
+
 namespace spiel
 {
     class CharacterEditor
     {
-        
+    public:
+        static void main(sf::RenderWindow& window, tgui::Gui& gui);
+    
+    private:
     };
 }
